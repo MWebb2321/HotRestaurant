3f4b1bf758c8657d41a9c6f1f70bdb4118919aec
Matthew Webb <mwebb2321@gmail.com>
2021-03-04 19:56:55 -0800

    combined js files

--- java.js
+++ java.js
@@ -13,3 +13,15 @@ app.post('/api/tables', (req, res) => {
     tables.push(newTable);
     res.json(newTable);
   });
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const waitlist = [
+    {
+    }
+];
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
